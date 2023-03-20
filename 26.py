02a02c4502aff3ad7fbb6c2f723dd62b255cf4e1
olegastra-git <qwertyray1995@gmail.com>
2023-03-20 22:54:23 +0300

    Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
    *Пример:*
    
    A = 3; B = 5 -> 243 (3⁵)
        A = 2; B = 3 -> 8

---
+++ 26.py
@@ -0,0 +1,12 @@
+from unittest import result
+
+
+a = int(input(" vvedite chislo a: - "))
+b = int(input(" vvedite chislo b: - "))
+def power(a,b):
+    if b == 0:
+        return 1
+    else:
+        return a* power(a, b-1)
+result=power(a,b)
+print(result)
